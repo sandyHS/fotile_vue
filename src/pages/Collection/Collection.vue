fe919f02bcfb1503cbe14001843e9c8f4cf2ac69
沈翔 <863461783@qq.com>
2017-04-25 18:06:39 +0800

    安全／收藏

--- src/pages/Collection/Collection.vue
+++ src/pages/Collection/Collection.vue
@@ -1,6 +1,85 @@
 <template>
   <div class="fotile_collection">
     <TitleWithNTALKER title="我的收藏"></TitleWithNTALKER>
+    <ul class="collectionList">
+      <li class="item">
+        <a href="javascript:void(0)">
+          <i class="iconfont icon-shoucang shoucang"></i>
+          <img src="http://img001.efotile.com/201741312547875.JPG" class="goodImg">
+          <div class="text-wrapper">
+            <p class="goodName">方太 JQ22TS＋HC26BE 风魔方 近吸直排 不跑烟 烟灶套餐（不带装饰罩）</p>
+            <p class="goodDesc">产品描述+产品描述+产品描述+产品描述</p>
+            <p class="price">6888.00</p>
+          </div>
+        </a>
+      </li>
+      <li class="item">
+        <a href="javascript:void(0)">
+          <i class="iconfont icon-shoucang shoucang"></i>
+          <img src="http://img001.efotile.com/201741312547875.JPG" class="goodImg">
+          <div class="text-wrapper">
+            <p class="goodName">方太 JQ22TS＋HC26BE 风魔方 近吸直排 不跑烟 烟灶套餐（不带装饰罩）</p>
+            <p class="goodDesc">产品描述+产品描述+产品描述+产品描述</p>
+            <p class="price">6888.00</p>
+          </div>
+        </a>
+      </li>
+      <li class="item">
+        <a href="javascript:void(0)">
+          <i class="iconfont icon-shoucang shoucang"></i>
+          <img src="http://img001.efotile.com/201741312547875.JPG" class="goodImg">
+          <div class="text-wrapper">
+            <p class="goodName">方太 JQ22TS＋HC26BE 风魔方 近吸直排 不跑烟 烟灶套餐（不带装饰罩）</p>
+            <p class="goodDesc">产品描述+产品描述+产品描述+产品描述</p>
+            <p class="price">6888.00</p>
+          </div>
+        </a>
+      </li>
+      <li class="item">
+        <a href="javascript:void(0)">
+          <i class="iconfont icon-shoucang shoucang"></i>
+          <img src="http://img001.efotile.com/201741312547875.JPG" class="goodImg">
+          <div class="text-wrapper">
+            <p class="goodName">方太 JQ22TS＋HC26BE 风魔方 近吸直排 不跑烟 烟灶套餐（不带装饰罩）</p>
+            <p class="goodDesc">产品描述+产品描述+产品描述+产品描述</p>
+            <p class="price">6888.00</p>
+          </div>
+        </a>
+      </li>
+      <li class="item">
+        <a href="javascript:void(0)">
+          <i class="iconfont icon-shoucang shoucang"></i>
+          <img src="http://img001.efotile.com/201741312547875.JPG" class="goodImg">
+          <div class="text-wrapper">
+            <p class="goodName">方太 JQ22TS＋HC26BE 风魔方 近吸直排 不跑烟 烟灶套餐（不带装饰罩）</p>
+            <p class="goodDesc">产品描述+产品描述+产品描述+产品描述</p>
+            <p class="price">6888.00</p>
+          </div>
+        </a>
+      </li>
+      <li class="item">
+        <a href="javascript:void(0)">
+          <i class="iconfont icon-shoucang shoucang"></i>
+          <img src="http://img001.efotile.com/201741312547875.JPG" class="goodImg">
+          <div class="text-wrapper">
+            <p class="goodName">方太 JQ22TS＋HC26BE 风魔方 近吸直排 不跑烟 烟灶套餐（不带装饰罩）</p>
+            <p class="goodDesc">产品描述+产品描述+产品描述+产品描述</p>
+            <p class="price">6888.00</p>
+          </div>
+        </a>
+      </li>
+      <li class="item">
+        <a href="javascript:void(0)">
+          <i class="iconfont icon-shoucang shoucang"></i>
+          <img src="http://img001.efotile.com/201741312547875.JPG" class="goodImg">
+          <div class="text-wrapper">
+            <p class="goodName">方太 JQ22TS＋HC26BE 风魔方 近吸直排 不跑烟 烟灶套餐（不带装饰罩）</p>
+            <p class="goodDesc">产品描述+产品描述+产品描述+产品描述</p>
+            <p class="price">6888.00</p>
+          </div>
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
--- src/pages/Security/Security.vue
+++ src/pages/Security/Security.vue
@@ -1,12 +1,38 @@
 <template>
-  <div class="fotile_security page-container">
-    用户安全
+  <div class="fotile_security">
+    <TitleWithNTALKER title="安全中心"></TitleWithNTALKER>
+    <div class="security">
+      <div class="security_ball">
+        <div class="text-title">安全级别</div>
+        <div class="text-grade">安全</div>
+      </div>
+      <p class="tips">建议您启动全部安全设置，以保障账户及资金安全</p>
+    </div>
+    <div class="security_items">
+      <div class="item password">
+        <div class="left-text">
+          <div class="inline black">
+            <i class="iconfont icon-duigou" style="color:#69c361;"></i>
+            登录密码
+          </div>
+          <div class="inline gray">
+            互联网账户存在被盗风险，建议您定期更改密码以保护账户安全
+          </div>
+        </div>
+        <div class="right-actions">
+          <a href="javascript:void(0)" class="button">修改密码</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import TitleWithNTALKER from '../../components/TitleWithNTALKER/TitleWithNTALKER.vue';
 export default {
-
+  components:{
+    TitleWithNTALKER
+  }
 }
 </script>
 
