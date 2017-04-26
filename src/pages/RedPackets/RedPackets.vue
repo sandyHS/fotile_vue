498ef312af9ca64a8c7d86ef89af37e9908024c2
sandyHS <elephant2015_hs@163.com>
2017-04-26 18:19:16 +0800

    redpack完成

--- src/pages/RedPackets/RedPackets.vue
+++ src/pages/RedPackets/RedPackets.vue
@@ -5,20 +5,92 @@
     	<li class="item">
         <div class="redpackinfo">
           <div class="redpack">
-            <span class="currency">￥</span>
+            <span class="currency">&yen</span>
             <span class="num">300</span>
           </div>
         </div>
-        <div>
+        <div class="expdate">
           <h3>方太商城<span>300</span>元电子红包</h3>
           <p>截止日期： 2018-01-01 00:00:00</p>
           <p>使用条件:   购买方太商城满7000可使用(秒杀、特惠商品及配件除外)仅限普通会员</p>
-          <input type="button" value="立即领取">
+          <a href="javascript:void(0)" class="receive">立即领取</a>
         </div>    
       </li>
-    	
-
+      <li class="item">
+        <div class="redpackinfo">
+          <div class="redpack">
+            <span class="currency">&yen</span>
+            <span class="num">300</span>
+          </div>
+        </div>
+        <div class="expdate">
+          <h3>方太商城<span>300</span>元电子红包</h3>
+          <p>截止日期： 2018-01-01 00:00:00</p>
+          <p>使用条件:   购买方太商城满7000可使用(秒杀、特惠商品及配件除外)仅限普通会员</p>
+          <a href="javascript:void(0)" class="receive">立即领取</a>
+        </div>    
+      </li>
     </ul>
+    <div class="myRedpack">
+      <h1>我的红包</h1>
+      <ul class="info">
+        <li class="item">
+          <div class="redpackinfo">
+            <div class="redpack">
+              <span class="currency">&yen</span>
+              <span class="num">300</span>
+            </div>
+          </div>
+          <div class="expdate">
+            <h3>方太商城<span>300</span>元电子红包</h3>
+            <p>截止日期： 2018-01-01 00:00:00</p>
+            <div class="condition clearfix">
+              <span>使用条件: </span>  
+              <span>购买方太商城满7000可使用</span>
+              <h6>(秒杀、特惠商品及配件除外)</h6>
+              <h6>仅限普通会员</h6>
+            </div>
+          </div> 
+        </li>
+         <li class="item">
+          <div class="redpackinfo">
+            <div class="redpack">
+              <span class="currency">&yen</span>
+              <span class="num">300</span>
+            </div>
+          </div>
+          <div class="expdate">
+            <h3>方太商城<span>300</span>元电子红包</h3>
+            <p>截止日期： 2018-01-01 00:00:00</p>
+            <div class="condition clearfix">
+              <span>使用条件: </span>  
+              <span>购买方太商城满7000可使用</span>
+              <h6>(秒杀、特惠商品及配件除外)</h6>
+              <h6>仅限普通会员</h6>
+            </div>
+          </div> 
+        </li>
+         <li class="item">
+          <div class="redpackinfo">
+            <div class="redpack">
+              <span class="currency">&yen</span>
+              <span class="num">300</span>
+            </div>
+          </div>
+          <div class="expdate">
+            <h3>方太商城<span>300</span>元电子红包</h3>
+            <p>截止日期： 2018-01-01 00:00:00</p>
+            <div class="condition clearfix">
+              <span>使用条件: </span>  
+              <span>购买方太商城满7000可使用</span>
+              <h6>(秒杀、特惠商品及配件除外)</h6>
+              <h6>仅限普通会员</h6>
+            </div>
+          </div> 
+        </li>
+ 
+      </ul>
+    </div>
   </div>
 </template>
 
--- src/pages/UserIntegral/UserIntegral.vue
+++ src/pages/UserIntegral/UserIntegral.vue
@@ -1,6 +1,31 @@
 <template>
-  <div class="fotile_userintegral page-container">
+  <div class="fotile_userintegral">
     <TitleWithNTALKER title="我的积分"></TitleWithNTALKER>
+    <div class="listwrap">
+    	<div class="currentIntegral">
+    		<span>当前积分</span>
+    		<span class="num">3000</span>
+    	</div>
+		<div class="integralList">
+			<ul>
+				<li class="title">
+					<span>获取日期</span>
+					<span>来源</span>
+					<span>+4000</span>
+				</li>
+				<li>
+					<span>2017-01-04 01:44:12</span>
+					<span>双11抽奖赠送1000积分</span>
+					<span>+4000</span>
+				</li>
+				<li>
+					<span>2017-01-04 01:44:12</span>
+					<span>双11抽奖赠送1000积分</span>
+					<span>+4000</span>
+				</li>
+			</ul>
+		</div>
+    </div>
   </div>
 </template>
 
